33f0a55bd1227e570ec0a1e132729c6942409270
Christine-horrocks <splogy2001@hotmail.com>
2018-11-23 15:35:40 +0000

    WEBDEV-893_SI_by_id_shows_both_layings

--- lib/parliament/grom/decorator/procedure_step.rb
+++ lib/parliament/grom/decorator/procedure_step.rb
@@ -3,6 +3,16 @@ module Parliament
     module Decorator
       # Decorator namespace for Grom::Node instances with type: https://id.parliament.uk/schema/ProcedureStep
       module ProcedureStep
+
+        # Alias procedureStepHasHouse with fallback.
+        #
+        # @return [Grom::Node, nil] the House of the Grom::Node or nil.
+        def house
+          return @house if @house
+          return nil unless respond_to?(:procedureStepHasHouse)
+
+          @house = Helpers::Utils.type_safe_first(procedureStepHasHouse, Parliament::Grom::Decorator::House)
+        end
       end
     end
   end
